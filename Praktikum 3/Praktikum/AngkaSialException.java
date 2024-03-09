050b006173b0196480cab7b29b439023ca237650
RakaMaulanaYusuf <128438426+RakaMaulanaYusuf@users.noreply.github.com>
2024-03-09 21:06:54 +0700

    Delete Praktikum 3/Praktikum directory

--- Praktikum 3/Praktikum/AngkaSialException.java
+++
@@ -1,8 +0,0 @@
-// NAMA : Raka Maulana Yusuf
-// NIM  : 24060122140119
-// File : AngkaSialException.java
-public class AngkaSialException extends Exception{
-    public AngkaSialException(){
-        super("Jangan memasukan angka 13 karena angka sial !!!");
-    }
-}
