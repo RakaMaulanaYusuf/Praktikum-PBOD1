98ea5fbe64e2f6b0cc6a2ab3cd951d572570455c
RakaMaulanaYusuf <128438426+RakaMaulanaYusuf@users.noreply.github.com>
2024-05-28 21:05:19 +0700

    Add files via upload

---
+++ PRAKTIKUM_10/Vehicle.java
@@ -0,0 +1,12 @@
+public abstract class Vehicle {
+
+    public abstract  double calcFuelEfficiency();
+
+    public abstract double calcTripDistance();
+
+    public String toString(){
+        return this.getClass().getSimpleName();
+    }
+    
+}
+
